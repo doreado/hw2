b1cc26f77603db67d5b8175feffec63cd736263a
doreado <edoardom59@tutanota.com>
2022-06-04 10:09:10 +0200

    Moved addUser in UsersController

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\UserPic;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Session;
@@ -23,6 +24,56 @@ class UsersController extends BaseController
 
     return ['id' => session()->get('user_id'),
         'username' => session()->get('username')];
+  }
+
+  public function addUser()
+  {
+    if (session()->has(['username', 'user_id'])) {
+      redirect('/home');
+    }
+
+    $request = request()->all();
+
+    // TODO VALIDATION
+
+    $user = new User;
+    $user->username = $request['username'];
+    $user->name = $request['name'];
+    $user->surname = $request['surname'];
+    $user->email = $request['email'];
+    $user->password = $request['password'];
+    $user->time = Carbon::now();
+    $user->save();
+
+    $user_pics = new UserPic;
+    $user_pics->user = $user->id;
+    $user_pics->profile_pic = file_get_contents($request['profile-image']);
+    $user_pics->cover_pic = file_get_contents($request['cover-image']);
+    $user_pics->save();
+
+    session(['user_id' => $user->id, 'username' => $user->username]);
+    return redirect('/home');
+  }
+
+  public function getPics(string $user_id)
+  {
+    if (session()->has(['username', 'user_id'])) {
+      redirect('/home');
+    }
+
+    $pics = UserPic::where('user', $user_id)->get()->first();
+
+    $res = [ 'profile_pic' => [
+                              'empty' => isset($pics->profile_pic) ? false : true,
+                              'src' => base64_encode($pics->profile_pic)
+                              ],
+               'cover_pic' => [
+                              'empty' => isset($pics->profile_pic) ? false : true,
+                              'src' => base64_encode($pics->profile_pic)
+                              ]
+    ];
+
+    return $res;
   }
 }
 ?>
