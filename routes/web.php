6573870f210fcd31fe07fbb9ba1fd6d71c0f9a90
doreado <edoardom59@tutanota.com>
2022-06-14 17:28:31 +0200

    Add default route

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,8 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/test/', function () {
-    return 'testest';
-});
+Route::get('/', function () { return redirect('login'); });
 
 Route::get('/signup', 'App\Http\Controllers\SignupController@show');
 
